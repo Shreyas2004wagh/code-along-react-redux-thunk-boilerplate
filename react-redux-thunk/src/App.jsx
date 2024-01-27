93b2cb7ae6d092f45b16ab26a3da251d8804a9a4
Shreyas2004wagh <shreyaswagh2004@gmail.com>
2024-01-27 16:29:44 +0530

    done

---
+++ react-redux-thunk/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import React from 'react'
+import './App.css'
+import DisplayData from './Redux/DisplayData'
+
+function App() {
+
+  return (
+    <>
+     <DisplayData/>
+    </>
+  )
+}
+
+export default App
---
+++ react-redux-thunk/src/Redux/Reducer.jsx
@@ -0,0 +1,20 @@
+const initState={
+    users:[],
+    error:''
+}
+
+export default function reducer(state=initState,{type,payload})
+{
+    switch(type)
+    {
+        case "FETCH_DATA":{
+            return {users:payload,error:""}
+        }
+        case "ERROR":{
+            return {users:[],error:payload}
+        }
+        default:{
+            return state;
+        }
+    }
+}
